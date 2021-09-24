403b86edab735a73054751a75119fd25ee0d26d7
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-09-24 15:21:51 +0200

    Update dags to clone

--- prepareDagsProd.sh
+++ prepareDagsProd.sh
@@ -1,2 +1,6 @@
 cd dags
-git clone git@gitlab.com:etalab/data-engineering/dag-publish-sirene-data.git
+git clone git@gitlab.com:etalab/data-engineering/dag_publish_sirene_data.git
+git clone git@gitlab.com:etalab/data-engineering/dag_geocodage_sirene.git
+git clone git@gitlab.com:etalab/data-engineering/dag_dashboard_aides_entreprises.git
+# Examples :
+# git clone git@gitlab.com:etalab/data-engineering/dag_examples_etalab.git
