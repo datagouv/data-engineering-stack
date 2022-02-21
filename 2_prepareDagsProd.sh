259523d4354565693f0dc0ae183eeac441b742ae
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-02-21 16:34:40 +0100

    Remove script

--- 2_prepareDagsProd.sh
+++
@@ -1,9 +0,0 @@
-cd dags
-git clone git@gitlab.com:etalab/data-engineering/dag_publish_sirene_data.git
-git clone git@gitlab.com:etalab/data-engineering/dag_geocodage_sirene.git
-git clone git@gitlab.com:etalab/data-engineering/dag_dashboard_aides_entreprises.git
-git clone git@gitlab.com:etalab/data-engineering/dag_notebooks.git
-git clone git@gitlab.com:etalab/data-engineering/dag_datatourisme_archive.git
-git clone git@gitlab.com:etalab/data-engineering/dag_cron.git
-# Examples :
-# git clone git@gitlab.com:etalab/data-engineering/dag_examples_etalab.git
--- README.md
+++ README.md
@@ -13,12 +13,8 @@ cd airflow-stack
 # Create directories necessary for Airflow to work
 ./1_prepareDirs.sh
 
-# Optionnal - Download actual dags used in production
-# Uncomment in script dag for examples if you want to play with test dags
-./2_prepareDagsProd.sh
-
 # Prepare .env file 
-./3_prepare_env.sh
+./2_prepare_env.sh
 nano .env 
 # Edit POSTGRES_USER ; POSTGRES_PASSWORD ; POSTGRES_DB ; AIRFLOW_ADMIN_MAIL ; AIRFLOW_ADMIN_FIRSTNAME ; AIRFLOW_ADMIN_NAME ; AIRFLOW_ADMIN_PASSWORD
 
