d3f61e6367bd4de4a36bb60445a712537fb5a7bc
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-05-17 11:29:13 +0200

    Update refreshDags script

--- refreshBagDags.sh
+++ refreshBagDags.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-
-echo docker exec -it "${PWD##*/}"_webserver_1 python -c "from airflow.models import DagBag; d = DagBag();"
-docker exec -it "${PWD##*/}"_webserver_1 python -c "from airflow.models import DagBag; d = DagBag();"
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+    echo docker exec -it "airflow-${AIRFLOW_ENV_TYPE}-${AIRFLOW_ENV_NAME}" python -c "from airflow.models import DagBag; d = DagBag();"
+    docker exec -it "airflow-${AIRFLOW_ENV_TYPE}-${AIRFLOW_ENV_NAME}" python -c "from airflow.models import DagBag; d = DagBag();"
+fi
