72a3af9a055086142273e1d62f7dbb7c3ac0425c
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2022-05-20 08:46:22 +0200

    style: fix black errors

--- plugins/operators/aio_color.py
+++ plugins/operators/aio_color.py
@@ -14,8 +14,8 @@ def get_next_color():
         response = requests.get(f"{AIO_URL}/colors")
         next_color = json.loads(response.content)["NEXT_COLOR"]
         response.raise_for_status()
-        logging.info(f'******************** AIO URL: {AIO_URL}/colors')
-        logging.info(f'******************** NEXT COLOR: {next_color}')
+        logging.info(f"******************** AIO URL: {AIO_URL}/colors")
+        logging.info(f"******************** NEXT COLOR: {next_color}")
         return next_color
     except requests.exceptions.RequestException as error:
         raise Exception("OOps: Error", error)
