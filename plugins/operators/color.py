a316f7ff4d3135f976a7656df4122a9aecbec839
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2022-06-03 15:58:56 +0200

    refactor: use airflow vars for env vars instead of .env files

--- plugins/operators/color.py
+++ plugins/operators/color.py
@@ -1,13 +1,10 @@
 import json
 import logging
 import os
+from airflow.models import Variable
 from urllib.request import urlopen
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-COLOR_URL = os.getenv("COLOR_URL")
+COLOR_URL = Variable.get("color_url")
 
 
 def get_next_color():
@@ -15,6 +12,7 @@ def get_next_color():
         with urlopen(COLOR_URL, timeout=5) as url:
             data = json.loads(url.read().decode())
             next_color = data["NEXT_COLOR"]
+            logging.info(f"******************** Color file URL: {COLOR_URL}")
             logging.info(f"******************** Next color from file: {next_color}")
             return next_color
     except BaseException as error:
