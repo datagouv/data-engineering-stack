166a511bf4a52f2aba7dfa7301d155c5e9eda613
Hajar AEK <48837850+HAEKADI@users.noreply.github.com>
2024-03-12 17:31:09 +0100

    Revert "feat: remove custom operators"

---
+++ plugins/operators/elastic_create_index.py
@@ -0,0 +1,84 @@
+import json
+import logging
+from typing import Optional
+
+import requests
+from airflow.models import BaseOperator
+
+
+class ElasticCreateIndexOperator(BaseOperator):
+    """
+    Create elasticsearch Index
+    :param elastic_url: endpoint url of elasticsearch
+    :type elastic_url: str
+    :param elastic_index: index to create
+    :type elastic_index: str
+    :param elastic_index_shards: number of shards for index
+    :type elastic_index_shards: int
+    :param elastic_user: user for elasticsearch
+    :type elastic_user: str
+    :param elastic_password: password for elasticsearch
+    :type elastic_password: str
+    """
+
+    supports_lineage = True
+
+    template_fields = (
+        "elastic_url",
+        "elastic_index",
+        "elastic_user",
+        "elastic_password",
+        "elastic_index_shards",
+    )
+
+    def __init__(
+        self,
+        *,
+        elastic_url: Optional[str] = None,
+        elastic_index: Optional[str] = None,
+        elastic_user: Optional[str] = None,
+        elastic_password: Optional[str] = None,
+        elastic_index_shards: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        self.elastic_url = elastic_url
+        self.elastic_index = elastic_index
+        self.elastic_user = elastic_user
+        self.elastic_password = elastic_password
+        self.elastic_index_shards = elastic_index_shards
+
+    def execute(self, context):
+        if not self.elastic_url:
+            raise ValueError("Please provide elasticsearch url endpoint")
+
+        try:
+            r = requests.delete(
+                self.elastic_url + self.elastic_index,
+                auth=(self.elastic_user, self.elastic_password),
+            )
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.RequestException,
+        ):
+            pass
+
+        if self.elastic_index_shards is not None:
+            settings = {
+                "settings": {"index": {"number_of_shards": self.elastic_index_shards}}
+            }
+        else:
+            settings = {}
+
+        headers = {"Content-Type": "application/json"}
+
+        r = requests.put(
+            self.elastic_url + self.elastic_index,
+            headers=headers,
+            data=json.dumps(settings),
+            auth=(self.elastic_user, self.elastic_password),
+        )
+
+        logging.info(r.json())
+        assert r.json()["acknowledged"] is True
---
+++ plugins/operators/python_minio.py
@@ -0,0 +1,80 @@
+import os
+from typing import Optional
+
+from airflow.operators.python import PythonOperator
+from minio import Minio
+
+
+class PythonMinioOperator(PythonOperator):
+    """
+    Executes a Python function and uploads contents of tmp_path to Minio.
+    :param tmp_path: tmp path to store report during processing
+    :type tmp_path: str
+    :param minio_url: minio url where report should be store
+    :type minio_url: str
+    :param minio_bucket: minio bucket where report should be store
+    :type minio_bucket: str
+    :param minio_user: minio user which will store report
+    :type minio_user: str
+    :param minio_password: minio password of minio user
+    :type minio_password: str
+    :param minio_output_filepath: complete filepath where to store report
+    :type minio_output_filepath: str
+    """
+
+    template_fields = (
+        "tmp_path",
+        "minio_url",
+        "minio_bucket",
+        "minio_user",
+        "minio_password",
+        "minio_output_filepath",
+        *PythonOperator.template_fields,
+    )
+
+    def __init__(
+        self,
+        *,
+        tmp_path: Optional[str] = None,
+        minio_url: Optional[str] = None,
+        minio_bucket: Optional[str] = None,
+        minio_user: Optional[str] = None,
+        minio_password: Optional[str] = None,
+        minio_output_filepath: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        self.tmp_path = tmp_path
+        self.minio_url = minio_url
+        self.minio_bucket = minio_bucket
+        self.minio_user = minio_user
+        self.minio_password = minio_password
+        self.minio_output_filepath = minio_output_filepath
+
+    def execute(self, context):
+        os.makedirs(os.path.dirname(self.tmp_path + "output/"), exist_ok=True)
+
+        super().execute(context)
+
+        client = Minio(
+            self.minio_url,
+            access_key=self.minio_user,
+            secret_key=self.minio_password,
+            secure=True,
+        )
+
+        # check if bucket exists.
+        found = client.bucket_exists(self.minio_bucket)
+        if found:
+            for path, subdirs, files in os.walk(self.tmp_path + "output/"):
+                for name in files:
+                    print(os.path.join(path, name))
+                    isFile = os.path.isfile(os.path.join(path, name))
+                    if isFile:
+                        client.fput_object(
+                            self.minio_bucket,
+                            self.minio_output_filepath
+                            + os.path.join(path, name).replace(self.tmp_path, ""),
+                            os.path.join(path, name),
+                        )
