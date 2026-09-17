2c0b6fad7cb2990e62e95a584e34bb18e1a77820
Sylvain Lesage <severo@rednegra.net>
2026-09-17 09:47:30 +0200

    chore: remove obsolete script refreshDagBags.sh

--- refreshBagDags.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-if [ -f .env ]; then
-    # Load Environment Variables
-    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
-    echo docker exec -it "airflow-${AIRFLOW_ENV_TYPE}-${AIRFLOW_ENV_NAME}" python -c "from airflow.models import DagBag; d = DagBag();"
-    docker exec -it "airflow-${AIRFLOW_ENV_TYPE}-${AIRFLOW_ENV_NAME}" python -c "from airflow.models import DagBag; d = DagBag();"
-fi
